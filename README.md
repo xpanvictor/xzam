dbdb410a11904c241e379c24d60c2b1541600859
xpan <xpanvictor@gmail.com>
2025-06-07 13:44:03 +0100

    feat(docs): Updates readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # xzam
+
+Implementation of shazam's paper. 
+### Usage
+```rs
+xzam // Gives you the cli commands to use
+```
+
+Read [here](https://medium.com/@xpanvictor/xzam-an-attempt-at-shazams-paper-84fe44901a7a)
--- src/fingerprint/fingerprint.rs
+++ src/fingerprint/fingerprint.rs
@@ -22,36 +22,6 @@ fn generate_hash(f1: usize, f2: usize, delta_t: usize) -> u32 {
     ((f1 << 22) | (f2 << 12) | delta_t) as u32
 }
 
-// pub fn generate_fingerprints(peaks: Vec<SpectralPeak>) -> PathBuf {
-
-//     peaks.par_iter().enumerate().for_each(|(idx, anchor)| {
-//         let mut buffer = Vec::new();
-//         let target_zone = peaks
-//             .iter()
-//             .skip(idx)
-//             .take_while(|p| p.time_index - anchor.time_index < TIME_INTERVAL_MAX);
-//         for target in target_zone {
-//             let p = Fingerprint {
-//                 hash: generate_hash(
-//                     anchor.freq_bin,
-//                     target.freq_bin,
-//                     target.time_index - anchor.time_index,
-//                 ),
-//                 offset: anchor.time_index as u32,
-//             };
-//             buffer.push(p);
-//         }
-
-//         // write to file
-//         let mut w = writer.lock().unwrap();
-//         for f in buffer {
-//             let line = serde_json::to_string(&f).unwrap();
-//             writeln!(&mut w, "{line}").unwrap();
-//         }
-//     });
-//     output_path.to_path_buf()
-// }
-
 /// Streaming fingerprint generator: yields fingerprints lazily
 pub fn generate_fingerprints<'a>(
     peaks: &'a [SpectralPeak],
